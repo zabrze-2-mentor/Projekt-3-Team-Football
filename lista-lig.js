a808eadc7d81e4c9baf8de5f4077a3fcffca12a0
Dominika Zarzecka <zarzeckadominika@gmail.com>
2019-11-22 21:31:01 +0100

    Dodano flagi

--- lista-lig.js
+++ lista-lig.js
@@ -6,8 +6,6 @@ function fetchFromAPI(url = undefined, authToken = undefined) {
 
         var xhttp = new XMLHttpRequest();
         xhttp.open("GET", url, true);
-        // xhttp.withCredentials = true;
-        // xhttp.setRequestHeader('Content-Type', 'application/json')
         if (authToken != undefined) {
             xhttp.setRequestHeader("X-Auth-Token", authToken);
         }
@@ -26,36 +24,49 @@ function fetchFromAPI(url = undefined, authToken = undefined) {
 async function fetchLeagues(plan = "TIER_ONE") {
     let response = await fetchFromAPI("http://api.football-data.org/v2/competitions?plan=" + plan, authToken);
     response = JSON.parse(response);
+    let counter = 1;
+    let divRow;
     response.competitions.forEach(async element => {
+        let cardContainer = document.getElementsByClassName("card-container")[0];
+        if (counter == 1) {
+            counter = 5;
+            divRow = document.createElement("div");
+            divRow.classList.add("row");
+            cardContainer.appendChild(divRow);
+        }
+
+        let divCol = document.createElement("div");
+        divCol.classList.add("col-lg-3");
+
 
         let divCard = document.createElement("div");
         divCard.classList.add("card");
-        divCard.classList.add("m-3");
 
-    //    element.area.name
-      
+
         let imgCard = document.createElement("img");
-        imgCard.src = "";
+        imgCard.classList.add("card-img-top");
+        imgCard.src = "img/card flag/" + element.area.name + ".png";
 
         let cardBody = document.createElement("div");
         cardBody.classList.add("card-body");
 
+
         let cardTitle = document.createElement("h5");
         cardTitle.classList.add("card-title");
 
+
+
         cardTitle.innerHTML = "<a href='stronaligi.html?id=" + element.id + "'>" + element.name + "</a>";
 
-        let cardContainer = document.getElementsByClassName("card-container")[0];
+        divRow.appendChild(divCol);
+        divCol.appendChild(divCard);
         divCard.appendChild(imgCard);
         cardBody.appendChild(cardTitle);
         divCard.appendChild(cardBody);
-console.log(element.area.name);
 
-        cardContainer.appendChild(divCard);
+        counter--;
     });
-    // console.log(response.competitions.area.name);
-    // console.log(response.competitions.name);
-    // console.log(response.competitions.emblemUrl);
+
 
 
 }
