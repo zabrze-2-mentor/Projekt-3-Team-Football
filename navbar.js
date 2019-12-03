c011fdb20021b74e960dc922ff127abc10b1ce9d
Mateusz W <warot.mateusz@onet.pl>
2019-12-03 23:10:01 +0100

    Navbar functionality

--- navbar.js
+++ navbar.js
@@ -1,69 +1,171 @@
-function addnavbar () {
-    // Dane które powinny być przekazywane ale nie są więc tu dla przykładu
-        const LeagueId = ['2013','2016','2021','2018','2001','2015','2002','2019','2003','2017','2014','2000'];
-        const LeagueName = ['Série A','England','Championship','Premier League','European Championship','UEFA Champions League','Ligue 1','Bundesliga','Serie A','Eredivisie','Primeira Liga','Primera Division','FIFA World Cup'];
-        const TeamId = ['Team1','Team2','Team3','Team4','Team5','Team6','Team7','Team8','Team9','Team10','Team11','Team12'];
-        const TeamName = ['Team1','Team2','Team3','Team4','Team5','Team6','Team7','Team8','Team9','Team10','Team11','Team12'];
-    // Bootstrap navbar   
+function addnavbar() {
+    window.onload = function () {
+        // Dane które powinny być przekazywane ale nie są więc tu dla przykładu
+        const LeagueId = [];
+        const LeagueName = [];
+        const TeamId = [];
+        const TeamName = [];
+        // Bootstrap navbar   
         const navbar = document.createElement('nav');
-        navbar.setAttribute('class','navbar navbar-expand-lg navbar-dark');
-        navbar.setAttribute('style','background-color:#1E352F')
-        navbar.setAttribute('id','navbar');
-        document.body.insertBefore(navbar, document.body.firstChild);
-    
-    // Bootstrap menu button
+        navbar.setAttribute('class', 'navbar navbar-expand-lg navbar-dark');
+        navbar.setAttribute('id', 'navbar');
+        document.body.insertBefore(navbar, document.body.childNodes[0]);
+
+        // Bootstrap menu button
         const button = document.createElement('button');
-        button.setAttribute('class','navbar-toggler');
-        button.setAttribute('type','button');
-        button.setAttribute('data-toggle','collapse');
-        button.setAttribute('data-target','#navbarSupportedContent');
-        button.setAttribute('aria-controls','navbarSupportedContent');
-        button.setAttribute('aria-expanded','false');
-        button.setAttribute('aria-label','Toggle navigation');
+        button.setAttribute('class', 'navbar-toggler');
+        button.setAttribute('type', 'button');
+        button.setAttribute('data-toggle', 'collapse');
+        button.setAttribute('data-target', '#navbarSupportedContent');
+        button.setAttribute('aria-controls', 'navbarSupportedContent');
+        button.setAttribute('aria-expanded', 'false');
+        button.setAttribute('aria-label', 'Toggle navigation');
         navbar.appendChild(button);
         button.innerHTML = '<span class="navbar-toggler-icon"></span>';
-    
-    // Bootstrap menu options  
+
+        // Bootstrap menu options  
         const divContainer = document.createElement('div');
-        divContainer.setAttribute('class','collapse navbar-collapse');
-        divContainer.setAttribute('id','navbarSupportedContent');
+        divContainer.setAttribute('class', 'collapse navbar-collapse');
+        divContainer.setAttribute('id', 'navbarSupportedContent');
         navbar.appendChild(divContainer);
-    
-    // Bootstrap navbar list
+
+        // Bootstrap navbar list
         const ul = document.createElement('ul');
         ul.setAttribute('class', 'navbar-nav mr-auto');
         divContainer.appendChild(ul);
-    
-    // Bootstrap league list elements
-        const liLeague = document.createElement('li');
-        liLeague.setAttribute('class','nav-item dropright');
-        liLeague.setAttribute('style','background-color:#335145');
-        liLeague.innerHTML = '<a class="nav-link dropdown-toggle" href="index.html" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Lista Lig </a>';
-        ul.appendChild(liLeague);
-    
-        const divLeague = document.createElement('div');
-        divLeague.setAttribute('class','collapse dropdown-menu');
-        divLeague.setAttribute('style','background-color:#828C51');
-        divLeague.setAttribute('aria-labelledby','navbarDropdown');
-    
-        for(i=0;i<LeagueId.length;i++) {
-        // League links
-            const league = document.createElement('li'); 
-            league.innerHTML = `<a class="dropdown-item dropdown-toggle" href="stronaligi.html${LeagueId[i]}" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">${LeagueName[i]}</a>`;
-            divLeague.appendChild(league);
-    
-            const divTeam = document.createElement('div');
-            divTeam.setAttribute('class',' dropright');
-            divTeam.setAttribute('aria-labelledby','navbarDropdown');
-            divTeam.setAttribute('style','background-color:#A6C36F');
-            league.appendChild(divTeam);
-        // Team links 
-            for(j=0;j<TeamId.length;j++) {
-                const team = document.createElement('li');
-                team.innerHTML = `<a class="dropdown-item" href="team.html${TeamId[j]}" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">${TeamName[j]}</a>`;
-                divTeam.appendChild(team);
-            }
+
+        //League API
+
+        function getLeagueNameAndId(leagueId, leagueName) {
+            fetch('http://api.football-data.org/v2/competitions?plan=TIER_ONE', {
+                    method: 'GET',
+                    headers: {
+                        "X-Auth-Token": 'f3e6d2495f604ee8b170b4282c503d9a'
+                    }
+                })
+                .then(res => res.json())
+                .then(data => {
+
+                    for (i = 0; i < data.competitions.length; i++) {
+                        leagueId[i] = data.competitions[i].id;
+                        leagueName[i] = data.competitions[i].name;
+                    }
+                    return leagueId, leagueName;
+                })
+                .catch(err => console.log(err))
         }
-        ul.firstChild.appendChild(divLeague);
+        getLeagueNameAndId(LeagueId, LeagueName);
+
+        console.log(LeagueId, LeagueName);
+
+        // Team API
+
+
+        function getTeamNameAndId(teamId, teamName) {
+            const leagueId = window.location.hash.slice(1);
+
+            fetch(`http://api.football-data.org/v2/competitions/${leagueId}/teams`, {
+                    method: 'GET',
+                    headers: {
+                        "X-Auth-Token": 'f3e6d2495f604ee8b170b4282c503d9a'
+                    }
+                })
+                .then(res => res.json())
+                .then(data => {
+
+                    for (j = 0; j < data.teams.length; j++) {
+                        teamId[j] = data.teams[j].id;
+                        teamName[j] = data.teams[j].name;
+                    }
+                    return teamId, teamName;
+                })
+                .catch(err => console.log(err))
+        }
+        getTeamNameAndId(TeamId, TeamName);
+
+        console.log(TeamId);
+        console.log(TeamName);
+
+
+        if (window.location.href.split('?')[0].includes('index.html')) {
+            // Bootstrap league list elements
+
+            const liLeague = document.createElement('li');
+            liLeague.setAttribute('class', 'nav-item dropdown');
+            liLeague.setAttribute('style', 'background-color:rgb(30, 53, 47)');
+            liLeague.innerHTML = '<a class="nav-link dropdown-toggle" href="index.html" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Leagues List </a>';
+            ul.appendChild(liLeague);
+
+            // Bootstrap menu div
+            setTimeout(function menuDivL() {
+                const divLeague = document.createElement('div');
+                divLeague.setAttribute('class', 'collapse dropdown-menu');
+                divLeague.setAttribute('style', 'background-color:rgb(130, 140, 81)');
+                divLeague.setAttribute('aria-labelledby', 'navbarDropdown');
+                liLeague.appendChild(divLeague);
+
+                for (i = 0; i < LeagueId.length; i++) {
+                    // League links
+                    let tmpLeagueId = LeagueId[i];
+                    console.log(LeagueId[i]);
+
+                    const league = document.createElement('li');
+                    league.setAttribute('class', 'dropdown-item');
+                    league.innerHTML = `<a class="nav-link" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">${LeagueName[i]}</a>`;
+                    league.addEventListener('click', function () {
+                        window.location.replace("stronaligi.html#" + tmpLeagueId)
+
+
+                    });
+                    divLeague.appendChild(league);
+                }
+                ul.firstChild.appendChild(divLeague);
+            }, 750);
+            menuDivL();
+        }
+
+        if (window.location.href.split('?')[0].includes('stronaligi.html')) {
+            // Bootstrap Team list elements
+
+            const liTeam = document.createElement('li');
+            liTeam.setAttribute('class', 'nav-item dropdown');
+            liTeam.setAttribute('style', 'background-color:rgb(30, 53, 47)');
+            liTeam.innerHTML = '<a class="nav-link dropdown-toggle" href="index.html" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Teams List </a>';
+            ul.appendChild(liTeam);
+
+            // Bootstrap menu div
+            setTimeout(function menuDivT() {
+                const divTeam = document.createElement('div');
+                divTeam.setAttribute('class', 'collapse dropdown-menu');
+                divTeam.setAttribute('style', 'background-color:rgb(130, 140, 81)');
+                divTeam.setAttribute('aria-labelledby', 'navbarDropdown');
+                liTeam.appendChild(divTeam);
+
+                for (j = 0; j < TeamId.length; j++) {
+                    // Team links
+                    let tmpTeamId = TeamId[j];
+
+
+                    const team = document.createElement('li');
+                    team.setAttribute('class', 'dropdown-item');
+                    team.innerHTML = `<a class="nav-link" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">${TeamName[j]}</a>`;
+                    divTeam.appendChild(team);
+                    team.addEventListener('click', function () {
+                        window.location.href = `team.html#${tmpTeamId}`;
+                    });
+                }
+                ul.firstChild.appendChild(divTeam);
+            }, 750);
+        }
+
+        // Bootstrap home
+        if (!window.location.href.split('?')[0].includes('index.html')) {
+            const liHome = document.createElement('li');
+            liHome.setAttribute('class', 'nav-item');
+            liHome.setAttribute('style', 'background-color:rgb(30, 53, 47)');
+            liHome.innerHTML = '<a class="nav-link" href="index.html"> Home <span class="sr-only">(current)</span></a>';
+            ul.appendChild(liHome);
+        }
     }
-    addnavbar();
+}
+addnavbar();
